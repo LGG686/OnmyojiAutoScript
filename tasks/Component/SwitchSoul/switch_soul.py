950145ede8a4e40d5af772d5d4cd7628e9ac059e
xylolit-mu <gf7004msi95@163.com>
2026-09-13 09:58:51 +0800

    调整(御魂切换)：限制装配点击并增加随机间隔

--- tasks/Component/SwitchSoul/switch_soul.py
+++ tasks/Component/SwitchSoul/switch_soul.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 # @author runhey
 # github https://github.com/runhey
+import random
 from time import sleep
 from typing import Union
 
@@ -115,11 +116,13 @@ class SwitchSoul(BaseTask, SwitchSoulAssets):
         # 2023.8.5 修改为无反馈的点击切换
         for i in range(2):
             self.click(target_click)
-            sleep(0.5)
+            if i < 1:
+                sleep(random.uniform(0.5, 1))
         # 点击队伍
         target_team = get_team_asset(team)
-        for i in range(3):
-            sleep(0.8)
+        for i in range(2):
+            if i > 0:
+                sleep(random.uniform(0.5, 1))
             self.screenshot()
             if self.appear(self.I_SOU_SWITCH_SURE):
                 while 1:
@@ -248,12 +251,14 @@ class SwitchSoul(BaseTask, SwitchSoulAssets):
         self.O_SS_TEAM_NAME.keyword = teamName
         while 1:
             self.screenshot()
-            if cnt_click >= 4:
+            if cnt_click >= 2:
                 break
             if self.appear_then_click(self.I_SOU_SWITCH_SURE, interval=0.8):
                 continue
             if self.ocr_appear_click_by_rule(self.O_SS_TEAM_NAME, self.I_SOU_CLICK_PRESENT, interval=1.5):
                 cnt_click += 1
+                if cnt_click < 2:
+                    sleep(random.uniform(0.5, 1))
                 continue
         logger.info(f'Switch soul_one group {groupName} team {teamName}')
 
